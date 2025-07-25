9f9c133038159ba4c897bb946da3cda6634590c6
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2025-07-24 20:32:11 -0500

    AGREGANDO EN CORS LA RUTA LOCAL DE MI FRONTEND PARA PROBAR SI FUNCIONA EL API-LEGASA

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
 const empresaRoutes = require("./routes/empresaRoutes");
+
 const app = express();
 
+const corsOptions = {
+  origin: ["'http://localhost:5173'"], 
+  methods: ["GET", "POST"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
 app.use("/api", empresaRoutes);
 
 
